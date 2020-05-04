204483f3df776a389675aad4eb7c4f198b5f575f
tmek1244 <tmek1244@users.noreply.github.com>
2020-05-04 14:02:30 +0200

    basic reservation side with changes in models (#5)
    * add home page
    
    * add simple home page and sign in page
    
    * basic profile and reservation side. Changed models

--- ReservationServiceProject/ReservationService/forms.py
+++ ReservationServiceProject/ReservationService/forms.py
@@ -1,5 +1,37 @@
 from django import forms
 
+from ReservationService.models import ClassroomReservation
+
 
 class OccupiedSlotsForm(forms.Form):
     class_name = forms.CharField(max_length=10)
+
+
+class ReservationForm(forms.ModelForm):
+    # class_name = forms.CharField(required=False)
+    TIME_CHOICES_BEGIN = (
+        ("1", "8:00"),
+        ("2", "9:35"),
+        ("3", "11:15"),
+        ("4", "12:50"),
+        ("5", "14:40"),
+        ("6", "16:15")
+    )
+    TIME_CHOICES_END = (
+        ("1", "9:30"),
+        ("2", "11:05"),
+        ("3", "12:45"),
+        ("4", "14:20"),
+        ("5", "16:10"),
+        ("6", "17:45")
+    )
+    start_of_booking = forms.ChoiceField(choices=TIME_CHOICES_BEGIN)
+    end_of_booking = forms.ChoiceField(choices=TIME_CHOICES_END)
+
+    class Meta:
+        model = ClassroomReservation
+        fields = ('class_name', )
+
+    def __init__(self, *args, **kwargs):
+        super(ReservationForm, self).__init__(*args, **kwargs)
+        self.fields['class_name'].required = False
